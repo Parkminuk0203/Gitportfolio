e2848390e9546ee57987f4fc214fa98ea840cce8
Minuk Park <parkminuk0203@gmail.com>
2022-04-25 16:33:58 +0900

    시험 공부

--- Python/변수의 개념과 기본 자료형.py
+++ Python/변수의 개념과 기본 자료형.py
@@ -71,7 +71,6 @@ print(x)
 
 # 파이썬의 예약어 확인
 import keyword
-keyword.kwlist
 print(keyword.kwlist)
 #['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 
 # 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 
